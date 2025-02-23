27929e533fc0f2dc715c555712012640486ddc6d
Bhavye Chauhan <bhavyechauhan@gmail.com>
2025-02-22 21:26:25 -0800

    got the motors to follow each other. Basically the figure configurations, didn't take too long

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -2,22 +2,24 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ElevatorConstants;
 
 public class ElevatorSubsystem extends SubsystemBase {
+
   // Motors & Encoder
-  private final SparkMax rightMotor = new SparkMax(ElevatorConstants.rightMotorID, MotorType.kBrushless);
   private final SparkMax leftMotor = new SparkMax(ElevatorConstants.leftMotorID, MotorType.kBrushless);
-  private final RelativeEncoder m_encoder = rightMotor.getEncoder();
+  private final SparkMax rightMotor = new SparkMax(ElevatorConstants.rightMotorID, MotorType.kBrushless);
+  private final RelativeEncoder m_encoder = leftMotor.getEncoder();
 
   private final SparkMaxConfig motorConfig = new SparkMaxConfig();
   //private final SparkBaseConfig motor1Config;
@@ -35,15 +37,32 @@ public class ElevatorSubsystem extends SubsystemBase {
       .positionConversionFactor(ElevatorConstants.CONVERSION_FACTOR)
       .velocityConversionFactor(getElevatorPositionMeters() / 60);
     
+    SparkMaxConfig globalConfig = new SparkMaxConfig();
+    SparkMaxConfig rightConfig = new SparkMaxConfig();
+
     
+    globalConfig
+        .smartCurrentLimit(50)
+        .idleMode(IdleMode.kBrake);
 
-    // Configure the follower motor
-    leftMotor.follow(rightMotor); // Follower motor mimics the lead motor
-    leftMotor.setInverted(true);   // Invert if needed (depends on physical setup)
+    // Apply the global config and invert since it is on the opposite side
+    rightConfig
+        .apply(globalConfig)
+        .inverted(true)
+        .follow(leftMotor);
+    /*
+     * Apply the configuration to the SPARKs.
+     *
+     * kResetSafeParameters is used to get the SPARK MAX to a known state. This
+     * is useful in case the SPARK MAX is replaced.
+     *
+     * kPersistParameters is used to ensure the configuration is not lost when
+     * the SPARK MAX loses power. This is useful for power cycles that may occur
+     * mid-operation.
+     */
+    leftMotor.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightMotor.configure(rightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-    // Configure motor settings (optional)
-    rightMotor.setSmartCurrentLimit(40);
-    leftMotor.setSmartCurrentLimit(40);
   }
 
   public double getElevatorPositionMeters() {
